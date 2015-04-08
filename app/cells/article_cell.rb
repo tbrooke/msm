46d836b32c5985a7ab13b08d48479e78390823ec
tbrooke <tom.brooke@gmail.com>
2015-04-08 11:00:23 -0400

    set up article cell

--- app/cells/article_cell.rb
+++ app/cells/article_cell.rb
@@ -1,6 +1,22 @@
 class ArticleCell < Cell::ViewModel
+   include PrismicHelper
+   include PrismicController
+
   def show
     render
   end
 
+  private
+
+  def title
+    model['article.title'].as_html_safe(link_resolver())
+  end
+
+  def link
+  link_to title, link_resolver().link_to(model) 
+  end
+
+  def lede
+    model['article.longlede'].as_html_safe(link_resolver())
+  end
 end
